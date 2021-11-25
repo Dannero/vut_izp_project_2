4e78be96775f1e778aed9603a2c0b523437a3acf
Dannero <denny.blasko@gmail.com>
2021-11-26 00:17:59 +0100

    Update setcal.c

--- setcal.c
+++ setcal.c
@@ -3,12 +3,40 @@
 #include <stdbool.h>
 #include <string.h>
 
-//const member_len = 30;
+#define member_len 30
+#define argument_count 2
 
-typedef struct {                //Structure for a universe member
-    char member[30];
+    //UNIVERSE STRUCTURES//
+typedef struct {      //Structure for a universe member
+    char *member;
     int member_num;
 } Universe_t;
+
+Universe_t *uni_array;   //Array of universe members
+
+
+    //RELATION STRUCTURES//
+typedef struct {     //Structure for a relation member 
+    int rel_x;
+    int rel_y;
+} Rel_mem;
+
+Rel_mem *rel_array;  //Array of relation members
+
+Rel_mem **multi_rel_arrays; //Array of relation arrays
+
+
+    //SET STRUCTURES//
+
+typedef struct {        //Structure for a set member
+    char *set_mem;
+    int set_mem_num;
+} Set_member;
+
+Set_member *set_array;  //Array of set members
+
+Set_member **multi_set_arrays; //Array of set arrays
+
 
 
 
@@ -16,38 +44,92 @@ typedef struct {                //Structure for a universe member
 
 int main(int argc, char* argv[]) 
 {
-    if (argc != 2) {
+    if (argc != argument_count) {   //Checking the correct number of arguments
         return EXIT_FAILURE;
-        fprintf(stderr,"Wrong argument count\n");
+        fprintf(stderr,"Error: Wrong argument count\n");
     }
 
     FILE *fp;
 
     fp = fopen(argv[1],"r");
+    //File opening check
+    if (fp == NULL) {
+        return EXIT_FAILURE;
+        fprintf(stderr,"Error: Opening file not successful\n");
+    }
 
     (void) argc;
     (void) argv;
-    int line_count = 1;
-    int line_pos = 0;
+    int line_count = 1;  //Number of lines
+    int line_pos = 0;    //Position in a line
     
-    while(1)
-    {
-      char c = fgetc(fp);
-      ++line_pos;
+    while( ! (feof(fp)) )  {
+        char c = fgetc(fp);
+        ++line_pos;
 
-      if (line_count == 1)&&(line_pos == 1)&&(c == 'U')) {
-          printf("Preslo\n");
-      }
+        if (line_pos == 1) {
 
-      if (c == '\n') {
-          line_count++;
-          line_pos = 0;
-      }
+            switch (c) {
 
-      if( feof(fp) ) {
-         break ;
-      }
-      printf("%c", c);
+                case 'U':               //Universe Function
+                    if (line_count == 1) {
+                        //universe_load_function
+                    }
+
+                    else {
+                        fprintf(stderr, "Error: Universe not at line 1\n");
+                        return EXIT_FAILURE;
+                    }
+                    break;
+                
+                case 'S':               //Set Function
+                    if (line_count != 1) {
+                        //set_load_function
+                    }
+
+                    else {
+                        fprintf(stderr, "Error: Set at line 1\n");
+                        return EXIT_FAILURE;
+                    }
+                    break;
+
+                case 'R':               //Relation Function 
+                    if (line_count != 1) {
+                        //relation_load_function
+                    }
+
+                    else {
+                        fprintf(stderr, "Error: Relation at line 1\n");
+                        return EXIT_FAILURE;
+                    }
+                    break;
+
+                case 'C':               //Command Function 
+                    if (line_count !=1) {
+                        //find_and_do_command
+                    }
+
+                    else {
+                        fprintf(stderr, "Error: Command at line 1\n");
+                        return EXIT_FAILURE;
+                    }
+                    break;
+
+                default:                //Invalid function
+                    fprintf(stderr, "Error: Invalid Function at line %d\n", line_count);
+                    return EXIT_FAILURE;
+                    break;
+            }
+        
+        
+        }
+
+        if (c == '\n') {
+            line_count++;
+            line_pos = 0;
+        }
+
+        printf("%c", c);
     
     }
 
