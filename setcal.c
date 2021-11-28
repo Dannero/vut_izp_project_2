d6f8fab0f6553f5dfe028e2a5061242616340c14
Dannero <denny.blasko@gmail.com>
2021-11-28 02:00:18 +0100

    Adding Work in progress Set loading function

--- setcal.c
+++ setcal.c
@@ -50,14 +50,16 @@ typedef struct {        //Array of set arrays
     int set_number;
 } Set_arrs;
 
-/*                                                      //NEED FIX
-int illegal_parse(char *llegal_strings, char *str) {
-    for (int i = 0; i < illegal_str_count; i++) {
-        if (strcmp(str, illegal_strings[i]) == 0)
-            return 0;
-    }
+
+
+int second_char_check(char *c, FILE **fp) {        //Checking if the second char in line is SPACE and passes through
+    *c = fgetc(*fp);                    //it to make input loading easier
+    if (*c != ' ') {
+        fprintf(stderr, "Error: Incorrect input syntax\n");
+        return 0;
+    }    
     return 1;
-} */
+}
 
         //LOADING THE UNIVERSE LINE//
 void load_uni_line(char *c, FILE **fp, bool *uni_load_fail, int *uni_mem_count) {        
@@ -67,11 +69,6 @@ void load_uni_line(char *c, FILE **fp, bool *uni_load_fail, int *uni_mem_count) 
                                                                       
     uni_array = malloc(sizeof(char));
 
-    *c = fgetc(*fp);
-    if (*c != ' ') {     //Checks if there's a non-space character after the U character
-        fprintf(stderr, "Error: Incorrect input syntax\n");       //Also gets rid of the space :)
-        *uni_load_fail = true;
-    }
 
     while (*c != '\n') {
         *c = fgetc(*fp);
@@ -83,7 +80,7 @@ void load_uni_line(char *c, FILE **fp, bool *uni_load_fail, int *uni_mem_count) 
         } 
 
         if (*c != ' ') {        //Loading chars into buffer while the char in file isn't a SPACE char
-            str_size++;
+            ++str_size;
             buffer = (char *) realloc(buffer, str_size * sizeof(char));                              
             buffer[str_size - 1] = *c;
             printf("String size: %d\n", str_size);
@@ -114,22 +111,61 @@ void load_uni_line(char *c, FILE **fp, bool *uni_load_fail, int *uni_mem_count) 
 
             printf("%s", *buffer);
 
-            ++str_count;
+            str_count++;
             str_size  = 0;
             free(buffer);
         }
 
     }
     *uni_mem_count = str_count; 
-    free(buffer);
-
 } 
 
 
 
-/*                                          //SET LINE LOADING WIP
-int load_set_line(char *c, FILE **fp) {
+                //!!!!!WORK IN PROGRESS!!!!!    SET LINE LOADING
+/*void load_set_line(char *c, FILE **fp, bool *set_load_fail, int uni_mem_count) {
+    char *buffer = malloc(sizeof(char));
+    int str_size = 0;
+    int str_count = 0;
+    Set_member_t set_member;
+    Set_t set_array;
 
+    while (*c != '\n') {
+        *c = fgetc(*fp);
+
+        if (*c != ' ') {
+            str_size++;
+            buffer = (char *) realloc (buffer, str_size * sizeof(char));
+            buffer[str_size - 1] = *c;
+        }
+
+        if (*c == ' ') {
+            for (int i = 0; i < uni_mem_count; i++) {
+                if (strcmp(buffer, uni_array[i]) != 0) {        //Checking for members outside the Universe
+                    fprintf(stderr, "Error: String does not belong into the Universe\n");
+                    *set_load_fail = true;
+                }
+                else set_member.set_mem_num = i;
+            }
+
+            for (int i = 0; i < str_count; i++) {               //Checking for repeating members
+                if (strcmp(buffer, set_array.member->set_mem[i]) == 0) {       
+                    fprintf(stderr, "Error: Multiple equal set members present\n");
+                    *set_load_fail = true;
+                }
+            }
+
+
+            set_member.set_mem = (char*) realloc(set_member.set_mem, str_size * sizeof(char));
+            set_array = (Set_t)realloc(set_array, ((str_count + 1) * sizeof(Set_member_t)));        //Incompatible NEED FIX
+            strcpy(set_array.member[str_count], buffer);
+            set_array.set_size = str_count + 1;
+
+        str_count++;
+        str_size = 0;
+        free(buffer);
+        }
+    }
 } */
     
 
@@ -166,14 +202,13 @@ int main(int argc, char* argv[])   {
 
                 case 'U':               //Universe Function
                     if (line_count == 1) {
-                        
-                        /*if (load_uni_line(&c, &fp) == -1)
-                            return EXIT_FAILURE;
-                        
-                        else uni_mem_count = (load_uni_line(&c, &fp)); */
 
-                        load_uni_line(&c, &fp, &uni_load_fail, &uni_mem_count);
-                        if (uni_load_fail == true) return EXIT_FAILURE;
+                        if (! second_char_check(&c, &fp)) 
+                            return EXIT_FAILURE;
+
+                        load_uni_line(&c, &fp, &uni_load_fail, &uni_mem_count); //Loading line into Universe arrays
+                        if (uni_load_fail == true) 
+                            return EXIT_FAILURE;
                     }
 
                     else {
@@ -184,6 +219,9 @@ int main(int argc, char* argv[])   {
 
                 case 'S':               //Set Function
                     if (line_count != 1) {
+
+                        if (! second_char_check(&c, &fp)) 
+                            return EXIT_FAILURE;
                         //set_load_function
                     }
 
@@ -228,7 +266,7 @@ int main(int argc, char* argv[])   {
         }
     }
 
-    for (int i = 0; i <= uni_mem_count; i++) {
+    for (int i = 0; i < uni_mem_count; i++) {
         printf("%s, ",uni_array[i]);
     }
 
